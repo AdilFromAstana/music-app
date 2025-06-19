f922bbe2cc0b6ca4e33c8871d641c8793c9a4457
adilFromAstana <adilfirstus@gmail.com>
2025-06-19 10:21:17 +0500

    d

--- src/pages/Main/HomePage.jsx
+++ src/pages/Main/HomePage.jsx
@@ -5,29 +5,50 @@ const { Title, Paragraph } = Typography;
 
 const HomePage = () => {
   return (
-    <div style={{ maxWidth: "800px", textAlign: "center", margin: "0 auto" }}>
-      <Title level={1} style={{ fontSize: "56px" }}>
-        Арқа әншілік мектебі
-      </Title>
-      <Title level={3} style={{ fontWeight: 400 }}>
-        Дәстүрлі ән мамандығы бойынша электронды оқу құралы
-      </Title>
-      <br />
-      <br />
-      <Title level={5}>
-        АВТОРЛАР <br />
+    <div
+      style={{
+        backgroundImage: "url('/dwa.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          maxWidth: "800px",
+          textAlign: "center",
+          margin: "0 auto",
+        }}
+      >
+        <Title level={1} style={{ fontSize: "56px" }}>
+          Арқа әншілік мектебі
+        </Title>
+        <Title level={3} style={{ fontWeight: 400 }}>
+          Дәстүрлі ән мамандығы бойынша электронды оқу құралы
+        </Title>
         <br />
-        Идея авторы: Қазақстанның еңбек сіңірген қайраткері, профессор — <br />
-        Алмасбек Нұрмаханұлы Алматов
         <br />
-        <br />
-        Құрастырушы автор: Республикалық байқаулардың лауреаты — <br />
-        Смағұлова Замзагүл Талғатқызы
-        <br />
-        <br />
-        Техникалық автор: веб-бағдарламашы — <br />
-        Айжанов Әділ Ерболұлы
-      </Title>
+        <Title level={5} style={{}}>
+          АВТОРЛАР <br />
+          <br />
+          Идея авторы: Қазақстанның еңбек сіңірген қайраткері, профессор —{" "}
+          <br />
+          Алмасбек Нұрмаханұлы Алматов
+          <br />
+          <br />
+          Құрастырушы автор: Республикалық байқаулардың лауреаты — <br />
+          Смағұлова Замзагүл Талғатқызы
+          <br />
+          <br />
+          Техникалық автор: веб-бағдарламашы — <br />
+          Айжанов Әділ Ерболұлы
+        </Title>
+      </div>
     </div>
   );
 };
